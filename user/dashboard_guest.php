22dc8a953aa9e3451ecaf6304604443d1a752a04
LaluYoga <laluyoga2704@gmail.com>
2025-06-22 22:15:48 +0800

    Pisahkan UI dahsboard guest dengan backend

--- user/dashboard_guest.php
+++ user/dashboard_guest.php
@@ -1,19 +1,5 @@
 <?php
-include '../includes/db.php';
-
-// Ambil beberapa circle publik
-$circles = $conn->query("SELECT name, description, created_at FROM circles ORDER BY created_at DESC LIMIT 5");
-
-// Ambil beberapa user aktif (yang punya posting)
-$users = $conn->query("
-    SELECT u.username, u.profession, u.city, COUNT(p.id) AS total_post
-    FROM users u
-    JOIN posts p ON u.id = p.user_id
-    GROUP BY u.id
-    ORDER BY total_post DESC
-    LIMIT 5
-");
-?>
+include '../backend/user/dahboard_guest_data.php' ?>
 
 <!DOCTYPE html>
 <html>
