bd9ac642188c4e81fbf0d2aedfad65279c7416de
LaluYoga <laluyoga2704@gmail.com>
2025-06-26 00:44:47 +0800

    Menyesuaikan modal info pesan pada halaman dikusi dengan teema gelap dan ukuran yang responsif

--- circle/discussion_page.php
+++ circle/discussion_page.php
@@ -162,11 +162,11 @@ include '../backend/circle/discussion_controller.php';
 
 <!-- Modal Info Pesan -->
 <div class="modal fade" id="messageInfoModal" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered modal-lg">
-    <div class="modal-content">
-      <div class="modal-header bg-info text-white">
-        <h5 class="modal-title">Info Pesan</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+  <div class="modal-dialog modal-dialog-centered modal-md modal-dialog-scrollable">
+    <div class="modal-content bg-dark text-white border border-secondary rounded-4">
+      <div class="modal-header bg-secondary text-white rounded-top-4">
+        <h5 class="modal-title"><i class="bi bi-info-circle me-2"></i>Info Pesan</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
       </div>
       <div class="modal-body" id="messageInfoContent">
         <div class="text-center text-muted">Memuat...</div>
